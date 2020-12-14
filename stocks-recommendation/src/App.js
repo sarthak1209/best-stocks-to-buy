81c5afae3492808f1277a4b1245b796737eebc59
Sarthak Lav <sarthakchaudhary52@gmail.com>
2020-12-14 23:27:48 +0530

    Added: styles

--- stocks-recommendation/src/App.js
+++ stocks-recommendation/src/App.js
@@ -1,21 +1,19 @@
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 
-
-var stocksDictionary ={
-  "Blue Chip": ["TCS", "HDFC Bank","Infosys", "ITC", "L&T" ],
-  "Dividend": ["Power Grid","NTPC","Coal India" , "ONGC", "IOCL"],
-  "Growth":["Relience", "Bajaj Finance", "Banaj Finserv", "Naukari"],
-  "My Other recommondations" :["Relaxo", "Bata", "Crompton", "VIP Bags"]
-}
+var stocksDictionary = {
+  "Blue Chip": ["TCS", "HDFC Bank", "Infosys", "ITC", "L&T"],
+  Dividend: ["Power Grid", "NTPC", "Coal India", "ONGC", "IOCL"],
+  Growth: ["Relience", "Bajaj Finance", "Banaj Finserv", "Naukari"],
+  "My Other recommondations": ["Relaxo", "Bata", "Crompton", "VIP Bags"]
+};
 
 var categories = Object.keys(stocksDictionary);
 
 export default function App() {
-
   var [stocks, setStocks] = useState("");
 
-  function onClickCategory(category){
+  function onClickCategory(category) {
     var stocks = stocksDictionary[category];
     console.log(stocks);
     setStocks(stocks);
@@ -25,20 +23,23 @@ export default function App() {
       <h1>Where will you invest after getting job by attending level 1?</h1>
       <h2>Here I present my favourite stocks!!</h2>
 
-      {
-        categories.map(category =>{
-          return <button onClick={()=> onClickCategory(category)} style={{margin:"2rem"}}>{category}</button>
-        })
-      }
+      {categories.map((category) => {
+        return (
+          <button
+            onClick={() => onClickCategory(category)}
+            style={{ margin: "2rem" }}
+          >
+            {category}
+          </button>
+        );
+      })}
+      <hr />
       <ul>
-      {
-          stocks.map((stock)=>{
-            return <li> {stock} </li>
-          })
-        }
+        {stocks &&
+          stocks.map((stock) => {
+            return <li> {stock} </li>;
+          })}
       </ul>
     </div>
   );
 }
-
-
